998b643297e3aab9c33e741cc711d2de5b5accb9
gayatri0605 <55155117+gayatri0605@users.noreply.github.com>
2020-04-23 11:15:50 -0500

    changes

--- for_students/grtown.js
+++ for_students/grtown.js
@@ -15,28 +15,69 @@
  * file to instantiate those objects in the world.
  */
 
+import * as T from "../libs/CS559-THREE/build/three.module.js";
 import { GrWorld } from "../libs/CS559-Framework/GrWorld.js";
+import {GrObject } from "../libs/CS559-Framework/GrObject.js";  // only for typing
 import * as Helpers from "../libs/CS559-Libs/helpers.js";
 import { WorldUI } from "../libs/CS559-Framework/WorldUI.js";
+import { SimpleHouse } from "../examples/house.js";
+import { CircularTrack, TrackCube, TrackCar } from "../examples/track.js";
+import { Helicopter, Helipad } from "../examples/helicopter.js";
+import { ShinySculpture } from "../examples/shinySculpture.js";
+import { MorphTest } from "../examples/morph.js";
+//import { GrTrees } from "../examples/myObj.js";
 
-import {main} from "../examples/main.js";
 
-/**m
- * The Graphics Town Main -
- * This builds up the world and makes it go...
- */
+
+
 function grtown() {
   // make the world
   let world = new GrWorld({
     width: 800,
     height: 600,
-    groundplanesize: 20 // make the ground plane big enough for a world of stuff
+    groundplanesize: 30 // make the ground plane big enough for a world of stuff
   });
 
   // put stuff into the world
-  // this calls the example code (that puts a lot of objects into the world)
-  // you can look at it for reference, but do not use it in your assignment
-  main(world);
+  
+  for (let i = -19; i < 20; i += 5) {
+    world.add(new SimpleHouse({ x: i, z: -12 }));
+    world.add(new SimpleHouse({ x: i, z: 12 }));
+  }
+
+  /** Race Track - with three things racing around */
+  let track = new CircularTrack();
+  let tc1 = new TrackCube(track);
+  let tc2 = new TrackCube(track);
+  let tc3 = new TrackCar(track);
+
+  // place things are different points on the track
+  tc2.u = 0.25;
+  tc3.u = 0.125;
+  // and make sure they are in the world
+  world.add(track);
+  world.add(tc1);
+  world.add(tc2);
+  world.add(tc3);
+
+  /** Helicopter - first make places for it to land*/
+  world.add(new Helipad(-15, 0, 0));
+  world.add(new Helipad(15, 0, 0));
+  world.add(new Helipad(0, 0, -17));
+  world.add(new Helipad(0, 0, 17));
+  let copter = new Helicopter();
+  world.add(copter);
+  copter.getPads(world.objects);
+
+  //trees
+  //world.add(new GrTrees({x: -25, z: 10, s: 1.5}));
+  //world.add(new GrTrees({x: -25, z:-10, s: 1.5}));
+
+  // these are testing objects
+  world.add(new ShinySculpture(world));
+  world.add(new MorphTest({ x: 10, y: 3, r: 2 }));
+
+  
 
   // build and run the UI
 
